b9a4ed6c1e1852bc2011d789e9f1db70c5b2b3e7
Ellie22302 <146311968+Ellie22302@users.noreply.github.com>
2024-04-09 15:07:54 -0400

    test

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,45 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge() {}
+//   if (input.license !== "none"){
+//     return `![Github license](https://img.sheilds.io/badge/license-${license}-yellowgreen.svg)`;
+//   }
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//   if (license !== "none"){
+//     return `\n* [License](#license)\n`;
+// }
+// }
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {
+// if (license !== "none"){
+//   return
+// }
+// }
 // TODO: Create a function to generate markdown for README
+const input = require("inquirer/lib/prompts/input");
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${input.title}
+  by ${input.name}
+  ${(input.license)}
+  ${(input.license)}
+  ##Description
+  ${input.description}
+  ## Requirements
+  ${input.require}
+  ##Usage
+  ${input.usage}
+  ##Contact Me
+  * Name - ${input.name}
+  * Email - ${input.email}
+  * Github - [${input.creator}](https://github.com/${input.creator}/)
+  ## Contributors
+  ${input.contributors}
+  ##Testting
+  ${input.test}
+  ${(input.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,47 +1,80 @@
-const fs = require("fs")
+const fs = require("fs");
 var inquirer = require('inquirer');
-const generateMarkdown = require("./generateMarkdown")
-inquirer
-  .prompt([
-   {
-    type: 'input',
-    name: 'input_type',
-    message: 'What is your name?',
-    default: 'Ellie'
-   },
-   {
-    type: 'list',
-    name: 'list question',
-    message: 'What language do you like?', 
-    choices:['Javascript','CSS','Html','SQL'],
-    default:'Javascipt'
-   },
-   {
-    type: 'checkbox',
-    name: 'checkbox question',
-    message: 'What languages do you like?', 
-    choices:['Javascript','CSS','Html','SQL'],
-    default:'Javascript'
-   },
-  ])
-  .then((answers) => {
-    console.log(answers)
-  })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   });
-// // TODO: Create an array of questions for user input
-// const questions = [];
+const generateMarkdown = require("./generateMarkdown");
+const path = require("path");
+function init(){
+inquirer.prompt([
+  {
+    type: "input",
+    name:"title",
+    message:"Name your Readme/Project",
+  },
+  {
+    type: "input",
+    name:"description",
+    message:"Please Describe your Readme/Project",
+  },
+  {
+    type: "Checkbox",
+    name:"license",
+    message:"Please select the licence for your Readme/Project",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name:"require",
+    message:"Please List any dependencies for your Readme/Project",
+  },
+  {
+    type: "input",
+    name:"usage",
+    message:"What languages or Technologies are used your Readme/Project?",
+  },
+  {
+    type: "input",
+    name:"name",
+    message:"What is your Name?",
+  },
+  {
+    type: "input",
+    name:"email",
+    message:"What is your Email?",
+  },
+  {
+    type: "input",
+    name:"github",
+    message:"What is your github username?",
+  },
+  {
+    type: "input",
+    name:"contributors",
+    message:"Who else helped you with this project (please use github usernames)?",
+  },
+  {
+    type: "input",
+    name:"test",
+    message:"What testing was done?",
+  },
+]).then((data) =>{
+const fileName = `${data.title}.md`;
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
+fs.writeFile(fileName, generateMarkdown(data)),(err)=>{
+  if (err) {
+    return console.log(err);}
+    console.log("file created")
+}})};
+// TODO: Create a function to write README file
+// function writeFile(fileName, generateMarkdown(data)) {
+//   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// }
 // // TODO: Create a function to initialize app
-// function init() {}
+// function init() {
 
-// // Function call to initialize app
-// init();
+//   inquirer.prompt(questions).then((responses) =>{
+//     console.log("Creating your README.md File");
+//     writeFile(".././Assets/readme.md", generateMarkdown({...responses}));
+//   });
+// }
+
+// Function call to initialize app
+init();
